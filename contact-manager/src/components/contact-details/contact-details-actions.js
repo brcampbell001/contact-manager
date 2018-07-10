707d110b0f0d4d26008e249f532d251df3b70dc9
unknown <brcampbell001@outlook.com>
2018-07-09 23:26:34 -0500

    Contact details actions added

---
+++ contact-manager/src/components/contact-details/contact-details-actions.js
@@ -0,0 +1,42 @@
+import { set, asyncUpdate, update } from "reduxigen";
+
+const base = "currentContact";
+const address = `${base}.address`;
+export const setEmail = set(`${base}.email`);
+export const setPhone = set(`${base}.phone`);
+export const setStreet = set(`${address}.street`);
+export const setSuite = set(`${address}.suite`);
+export const setCity = set(`${address}.city`);
+export const setZip = set(`${address}.zipcode`);
+export const setSuccess = set("actionSuccess");
+
+export const setName = update("name", (name, state) => ({
+  currentContact: {
+    ...state.currentContact,
+    name,
+    test: state.currentContact.phone
+  }
+}));
+
+const CONTENT_TYPE = "application/json; charset=UTF-8";
+
+const putContact = contact => {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${contact.id}`, {
+    method: "PUT",
+    body: JSON.stringify(contact),
+    headers: {
+      "Content-type": CONTENT_TYPE
+    }
+  });
+};
+
+const setContactData = data => ({
+  currentContact: data,
+  actionSuccess: true
+});
+
+export const updateContact = asyncUpdate(
+  "currentContact",
+  putContact,
+  setContactData
+);

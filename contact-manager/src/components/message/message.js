ba43fc2c806b99a9012b380102d0c9374f10f02b
unknown <brcampbell001@outlook.com>
2018-07-09 23:33:03 -0500

    Message success component + css added

---
+++ contact-manager/src/components/message/message.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+import { setSuccess } from "../contact-details/contact-details-actions";
+import "./message.css";
+
+export const Message = props => (
+  <div
+    className={`message message-${props.type} ${
+      props.actionSuccess ? "show" : "hide"
+    }`}
+  >
+    <div onClick={() => props.setSuccess(false)} className="message-close">
+      x
+    </div>
+    <h4>{props.content}</h4>
+    <textarea
+      className="message-contact"
+      disabled
+      value={JSON.stringify(props.contact)}
+    />
+  </div>
+);
+
+const mapStateToProps = state => ({
+  actionSuccess: state.actionSuccess
+});
+
+export default connect(
+  mapStateToProps,
+  { setSuccess }
+)(Message);
